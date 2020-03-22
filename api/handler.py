bd55c5f4bc03b9b743051a3b98e308c8763debee
Jose Rivera <josemrivera@gmail.com>
2020-03-22 02:25:36 +0000

    Add template feeding from swagger spec and lambda stubs.

--- api/handler.py
+++ api/handler.py
@@ -1,24 +1,34 @@
 import json
 
 
-def hello(event, context):
+def postLocation(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "POST /location endpoint logic. Your function executed successfully!",
+        "input": event,
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+
+def postInfected(event, context):
+    body = {
+        "message": "POST /infected endpoint logic! Your function executed successfully!",
+        "input": event,
     }
-    """
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+
+def getDevicesNearInfected(event, context):
+    body = {
+        "message": "GET /devices-near-infected endpoint logic! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
