c427d3bc6718725fb153b30f93e1a5facb840aba
DC <cytermann@gmail.com>
2020-03-22 11:40:03 -0500

    Implemented first version of notifications Added internationalization for notification messages

--- db_schemas/sandbox.sql
+++ db_schemas/sandbox.sql
@@ -1,28 +1,44 @@
 
 insert into contact_tracer.device (device_id ,first_sample ,latest_sample)
-values ('device1', now(),now()),('device2', now(),now()),('device3', now(),now())
-  
+values 
+('device1', now(),now()),
+('device2', now(),now()),
+('device3', now(),now())
+ on conflict do nothing ; 
 
 SELECT * FROM contact_tracer.device;
 
 
 truncate contact_tracer.device_location ;
 INSERT INTO contact_tracer.device_location (device_id,sample_date,location) VALUES 
-('device1', now(), Point(29.733521,-95.388815)),
-('device1', now() - interval '2 hours', Point(29.733730, -95.387850)),
-('device1', now() - interval '3 hours', Point(29.734117, -95.387850)),
-('device1', now() - interval '4 hours', Point(29.734410, -95.388113)),
-('device2', now() - interval '2 hours', Point(29.7344752,-95.3882035)),
-('device3', now() - interval '3 days', Point(29.7344752,-95.3882035));
+('device1', now(), ST_MakePoint(29.733521,-95.388815)),
+('device1', now() - interval '2 hours', ST_MakePoint(29.733730, -95.387850)),
+('device1', now() - interval '3 hours', ST_MakePoint(29.734117, -95.387850)),
+('device1', now() - interval '4 hours', ST_MakePoint(29.734410, -95.388113)),
+('device2', now() - interval '2 hours', ST_MakePoint(29.7344752,-95.3882035)),
+('device3', now() - interval '3 days', ST_MakePoint(29.7344752,-95.3882035));
 
 
 
+
+CREATE FUNCTION abs(interval) RETURNS interval AS
+  $$ select case when ($1<interval '0') then -$1 else $1 end; $$
+LANGUAGE sql immutable;
 
 --device1's user is infected, look at his history for the past 14 days (incubation period) 
 -- for each reported location of device1, find the devices that were near him at that time
 
-select dl.*,dl3.device_id, dl3.sample_date from contact_tracer.device_location dl
-left join lateral (select * from contact_tracer.device_location dl2 where ST_DistanceSphere(dl.location,dl2.location) < 50) dl3
+
+select ST_DistanceSphere(ST_MakePoint(37.842245, -122.261125),ST_MakePoint(37.842012, -122.261388))
+;
+
+
+select distinct dl3.device_id as notifyDeviceId
+from 
+contact_tracer.device_location dl join lateral 
+(select * from contact_tracer.device_location where device_id != dl.device_id and ST_DistanceSphere(location,dl.location) < 500
+and abs(dl.sample_date - sample_date) <= interval '1 hour'
+) dl3
 on true 
-where device_id  = 'device1'
-and sample_date > now() - interval '14 days';
+where dl.device_id  = 'device1'
+and dl.sample_date > now() - interval '14 days';
