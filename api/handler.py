11995a5d43ee0bc6b2ac6300cbd873e64f61f6fc
DC <cytermann@gmail.com>
2020-03-22 14:37:06 -0500

    added device score API and scheduled lambda to calculate it

--- api/handler.py
+++ api/handler.py
@@ -47,6 +47,22 @@ def postLocation(event, context):
     return response
 
 
+def getDevicesExposed(deviceId,cursor) :
+    cursor.execute(f"""
+        select distinct dl3.device_id as notifyDeviceId
+        from 
+        contact_tracer.device_location dl 
+        join lateral 
+        (
+            select * from contact_tracer.device_location where device_id != dl.device_id and ST_DistanceSphere(location,dl.location) < 10
+            and abs(dl.sample_date - sample_date) <= interval '1 hour'
+            ) dl3 on true 
+            where dl.device_id  = '{deviceId}'
+            and dl.sample_date > now() - interval '14 days';
+        """)
+    return [row[0] for row in cursor.fetchall()]
+
+
 def postInfected(event, context):
     # get db connection
     print(f"processing infected {event}")
@@ -56,24 +72,11 @@ def postInfected(event, context):
     cursor.execute(
         f"update contact_tracer.device set infected = true, infected_date = '{event['infectionDate']}' where device_id = '{event['deviceId']}'; ")
     # get all devices that need to be notified
-    cursor.execute(f"""
-    select distinct dl3.device_id as notifyDeviceId
-    from 
-    contact_tracer.device_location dl 
-    join lateral 
-    (
-        select * from contact_tracer.device_location where device_id != dl.device_id and ST_DistanceSphere(location,dl.location) < 500
-        and abs(dl.sample_date - sample_date) <= interval '1 hour'
-        ) dl3 on true 
-        where dl.device_id  = '{event['deviceId']}'
-        and dl.sample_date > now() - interval '14 days';
-    """)
-
-    deviceIdsToNotify = cursor.fetchall()
+    deviceIdsToNotify = getDevicesExposed(event['deviceId'],cursor)
     print(f"the following devices need to be notified : {deviceIdsToNotify}")
     # send notification for each
     for deviceId in deviceIdsToNotify:
-        notifyDevice(deviceId[0], i18n.t("wording.infected-notification-title"), i18n.t("wording.infected-notification-body"))
+        notifyDevice(deviceId, i18n.t("wording.infected-notification-title"), i18n.t("wording.infected-notification-body"))
 
     cursor.execute(
         f"update contact_tracer.device set notification_sent = true where device_id = '{event['deviceId']}'; ")
@@ -87,6 +90,7 @@ def postInfected(event, context):
 
     return response
 
+
 def getDevicesNearInfected(event, context):
     body = {
         "message": "GET /devices-near-infected endpoint logic! Your function executed successfully!",
@@ -96,3 +100,53 @@ def getDevicesNearInfected(event, context):
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
+
+
+def getDeviceRisk(event, context):
+    conn = make_conn()
+    cursor = conn.cursor()
+    deviceId = event['deviceId']
+    body = {
+        "deviceId": deviceId,
+        "score": getLatestDeviceScore(deviceId, cursor)
+    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
+    return response
+
+
+def calculateScore(deviceId, cursor):
+    exposedDevices = getDevicesExposed(deviceId, cursor)
+    if not exposedDevices:
+        return 1
+    if len(exposedDevices) == 1:
+        return 2
+    if len(exposedDevices) > 1:
+        return 3
+
+
+def getLatestDeviceScore(deviceId, cursor):
+    cursor.execute(f"select score from contact_tracer.device_risk where device_id = '{deviceId}';")
+    row = cursor.fetchone()
+    return row[0] if row else None
+
+
+def calculatRiskScores(event, context):
+    # get all devices
+    conn = make_conn()
+    cursor = conn.cursor()
+    cursor.execute("select device_id from contact_tracer.device order by latest_sample desc;")
+    devicesResponse = cursor.fetchall()
+    # iterate over them, calculate the device score, if available get the previous score
+    for row in devicesResponse :
+        deviceId = row[0]
+        score = calculateScore(deviceId, cursor)
+        prevScore = getLatestDeviceScore(deviceId, cursor)
+        if prevScore:
+            # if the score changed send a notification
+            if score != prevScore :
+                notifyDevice(deviceId, i18n.t("wording.score-changed-title"), i18n.t("wording.score-changed-body"))
+        # upsert the updated score
+        cursor.execute(
+            f"insert into contact_tracer.device_risk(device_id ,score ,last_calculated ) values('{deviceId}',{score},now()) on conflict(device_id) do update set score =EXCLUDED.score, last_calculated = EXCLUDED.last_calculated;")
+
+    return "done"
