425e9bd058be949bedafca7c2370e42152384402
Jose Rivera <josemrivera@gmail.com>
2020-03-22 19:32:42 +0000

    Almost there but cannot beat this beast...

--- api/handler.py
+++ api/handler.py
@@ -14,7 +14,7 @@ def make_conn():
     conn = None
     try:
         conn = psycopg2.connect(
-            f"dbname={DB_NAME} user={DB_USER} host={DB_HOST} password={DB_PASS}"
+            f"dbname={DB_NAME} user={DB_USER} host={DB_HOST} password={DB_PASS}  options='-c search_path={DB_SCHEMA}'"
         )
     except:
         print("I'm unable to connect to the database")
@@ -22,16 +22,18 @@ def make_conn():
 
 
 def postLocation(event, context):
-    # body = {
-    #     "message": "POST /location endpoint logic. Your function executed successfully!",
-    #     "input": event,
-    # }
-
-    # response = {"statusCode": 200, "body": json.dumps(body)}
+    payload = json.loads(event["body"])
+    print(payload)
+    point_str = f"POINT({payload['longitude']}, {payload['latitude']})"
+    print(point_str)
+    sql_statement = f"INSERT INTO contact_tracer.device_location (device_id,sample_date,location) VALUES ('{payload['deviceId']}', '{payload['sampleDate']}', ST_PointFromText('{point_str}'))"
+    print(sql_statement)
     conn = make_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT datname from pg_database;")
-    raw = cursor.fetchall()
+    cursor.execute(
+        sql_statement, (payload["deviceId"], payload["sampleDate"], point_str,),
+    )
+    conn.commit()
     response = {"statusCode": 200, "body": json.dumps(raw)}
     return response
 
